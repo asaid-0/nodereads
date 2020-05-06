56f15704c8e2a99dbd5ad19a86c8b3cb8f653534
mostafaElkenany <mostafa.elkenany@gmail.com>
2020-05-06 21:39:44 +0200

    -added routes for books

--- routes/books.js
+++ routes/books.js
@@ -1,37 +1,47 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.send('route get /books');
+})
+
 router.get('/:id', (req, res) => {
-    res.send('route get /books/:id');
+    const { id } = req.params
+    res.send(`route get /books/${id}`);
 })
 
 router.post('/:id', (req, res) => {
     const { type } = req.body;
+    const { id } = req.params
 
     if (type === 'review') {
-        res.send('post review');
+        res.send(`post review /book/${id}`);
     }
 
     else if (type === 'rate') {
-        res.send('post rate');
+        res.send(`post rate /book/${id}`);
     }
 
 })
 
 router.patch('/:id', (req, res) => {
     const { type } = req.body;
+    const { id } = req.params
 
     if (type === 'review') {
-        res.send('patch review');
+        res.send(`patch review /books/${id}`);
     }
 
 })
 
 router.delete('/:id', (req, res) => {
     const { type } = req.body;
+    const { id } = req.params
 
     if (type === 'review') {
-        res.send('delete review');
+        res.send(`delete review /books/${id}`);
     }
 
 })
+
+module.exports = router
