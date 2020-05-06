8a22bffa2c1c0dbbed82ac765ef1fcdfc2d9de65
moazGalbat <moaz.galbat@gmail.com>
2020-05-06 19:37:06 +0200

    some setupe to the server

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose')
+
+require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT || 5000
+const uri = process.env.DB_URI
 
-app.get('/api/customers', (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
 
-  res.json(customers);
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  res.json({"message": "working"});
 });
 
-const port = 5000;
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}, (err) => {
+    if (!err) console.log("mogodb started");
+    else console.log(err);
+})
 
-app.listen(port, () => `Server running on port ${port}`);
+app.listen(port, () => console.log(`Server running on port ${port}`));
