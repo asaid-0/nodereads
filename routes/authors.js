e6c8b473d25933ac3a95a5eb997f903afeb391f8
Mohamed Feeshar <mohamed.feeshar@gmail.com>
2020-05-08 22:26:31 +0200

    add authors route and get authors books

---
+++ routes/authors.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const AuthorModel = require('../models/Author')
+
+const router = express.Router();
+
+router.get("/:id/books", (req, res) => {
+    try {
+        author = AuthorModel.findById(req.params.id).populate("books").exec()
+        res.json(author)
+    } catch (error) {
+        res.send(error)
+    }
+})
