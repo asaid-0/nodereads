0e25c7cf4557454916fd68a9acaf6b8a69b4f989
Mohamed Feeshar <mohamed.feeshar@gmail.com>
2020-05-10 10:54:09 +0200

    removed non functional hook in Categroy model, add unique to name

--- models/Category.js
+++ models/Category.js
@@ -1,22 +1,12 @@
 const mongoose = require('mongoose');
 const schema = new mongoose.Schema({
-    name: { type: String, required: true, minlength: 3, maxlength: 15 },
+    name: { type: String, required: true, unique: true},
     books: [
         {
             type: mongoose.Schema.Types.ObjectId, ref: 'Book'
         }
     ]
 })
-
-schema.pre('remove', function (next) {
-    var category = this;
-    category.model('Book').update(
-        { categories: {$in: category.books}}, 
-        { $pull: { category: category._id } }, 
-        { multi: true }, 
-        next
-     );
-});
 
 const Category = mongoose.model('Category', schema);
 module.exports = Category;
