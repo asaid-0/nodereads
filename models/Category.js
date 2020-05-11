70ac7f937f23b2182d0b3c4e8aade39870ef3b38
Mohamed Feeshar <mohamed.feeshar@gmail.com>
2020-05-11 04:25:04 +0200

    remove books from category model

--- models/Category.js
+++ models/Category.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose');
 const schema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true},
-    books: [
-        {
-            type: mongoose.Schema.Types.ObjectId, ref: 'Book'
-        }
-    ]
+    name: { type: String, required: true, unique: true}
 })
 
 const Category = mongoose.model('Category', schema);
