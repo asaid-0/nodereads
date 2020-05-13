98ed07de360c91cef2eff35cb4fc4c8c5a7e9c1f
moazGalbat <moaz.galbat@gmail.com>
2020-05-14 01:27:49 +0200

    fix conflict

--- client/src/components/adminViews/AdminBooks.js
+++ client/src/components/adminViews/AdminBooks.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 import React, {useState, useEffect} from 'react'
 import { Container, Row, Col ,Table} from 'react-bootstrap';
 import BookRow from './BookRow'
 import axios from 'axios'
-=======
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap';
->>>>>>> 7e85e1974a3b5e4c1c2782c87ddaaf01c56cb51c
 
 function AdminBooks() {
         const [books, setBooks] = useState([]);
