c4437c5885d4ca1d42df655fe4c3e5f8a78407b3
YahyaWsea <yahya.wsea@gmail.com>
2020-05-14 21:12:03 +0200

    working on pagination

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Dashboard from './components/adminViews/Dashboard';
 import Home from './components/userViews/Home';
 import Books from './components/userViews/Books';
-import Book from './components/userViews/Book';
+import Book from './components/userViews/Book.jsx';
 import Authors from './components/userViews/Authors';
 import Categories from './components/userViews/Categories';
 import AdminBooks from './components/adminViews/AdminBooks';
@@ -18,23 +18,23 @@ function App() {
   return (
     <>
       <Router>
-        <Route exact path="/home" component={Home}/>
+        <Route exact path="/home" component={Home} />
 
         <Route exact path="/" component={Home} />
 
-        <Route exact path="/books" component={Books}/>
+        <Route exact path="/books" component={Books} />
         <Route exact path="/books/:bookId" component={Book} />
 
-        <Route exact path="/authors" component={Authors}/>
+        <Route exact path="/authors" component={Authors} />
 
 
-        <Route exact path="/categories" component={Categories}/>
+        <Route exact path="/categories" component={Categories} />
 
-        <Route exact path="/admin" component={Dashboard}/>
-        <Route exact path="/admin/books" component={AdminBooks}/>
-        <Route exact path="/admin/authors" component={AdminAuthors}/>
-        <Route exact path="/admin/authors/add" component={AuthorForm}/>
-        <Route exact path="/admin/authors/edit/:authorId" component={AuthorForm}/>
+        <Route exact path="/admin" component={Dashboard} />
+        <Route exact path="/admin/books" component={AdminBooks} />
+        <Route exact path="/admin/authors" component={AdminAuthors} />
+        <Route exact path="/admin/authors/add" component={AuthorForm} />
+        <Route exact path="/admin/authors/edit/:authorId" component={AuthorForm} />
 
       </Router>
     </>
--- client/src/components/userViews/BookCard.js
+++ client/src/components/userViews/BookCard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './BookCard.module.css';
 import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
 
+
 const BookCard = (props) => {
 
     const { item } = props;
@@ -12,17 +13,15 @@ const BookCard = (props) => {
 
         <Card className={styles.card}>
             <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-            <Card.Body>
+            <Card.Body className={styles.text}>
                 <Card.Title>{item.book.title}</Card.Title>
-                <Card.Text>
+                <Card.Text >
+                    {item.book.shortDescription}
                 </Card.Text>
             </Card.Body>
             <ListGroup className="list-group-flush">
                 <ListGroupItem>{item.shelf}</ListGroupItem>
             </ListGroup>
-            <Card.Body>
-                <Card.Link href="#">Book Details</Card.Link>
-            </Card.Body>
         </Card>
 
 
