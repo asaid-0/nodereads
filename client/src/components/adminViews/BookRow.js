ce27b827e90b467c898ad5879c493b7a6c556057
moazGalbat <moaz.galbat@gmail.com>
2020-05-20 00:36:30 +0200

    styling to bboks/authors table, enhance dashboard sidenav

--- client/src/components/adminViews/BookRow.js
+++ client/src/components/adminViews/BookRow.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import { Button , Badge } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import {
+    EditFilled,
+    DeleteFilled,
+} from '@ant-design/icons';
 
 function BookRow({ book, index, deleteBook }) {
     return (
         <tr>
             <td>{index + 1}</td>
+            <td><img style={{ width: 100, height: 100 }} src={book.photo ? `/${book.photo}` : ''} alt="book" /></td>
             <td>{book.name}</td>
             <td>{book.author.firstname}</td>
-            <td>{book.categories.map((cat,index)=><h3  key={index}><Badge pill variant="warning"> {cat.name} </Badge></h3>)}</td>
-
-            <td><img style={{ width: 200, height: 200 }} src={book.photo ? `/${book.photo}` : ''} alt="book" /></td>
+            <td>{book.categories.map((cat,index)=><h5  key={index}><Badge pill variant="warning"> {cat.name} </Badge></h5>)}</td>
             <td>
-                <Button variant="info" as={Link} to={`/admin/books/edit/${book._id}`}>Edit</Button>
-                <Button variant="danger" onClick={() => deleteBook(book._id)}>Delete</Button>
+                <Button variant="info" as={Link} to={`/admin/books/edit/${book._id}`}><EditFilled style={{ fontSize: '20px' }} /></Button><br/>
+                <Button variant="danger" onClick={() => deleteBook(book._id)}><DeleteFilled style={{ fontSize: '20px' }} /></Button>
             </td>
         </tr>
     )
