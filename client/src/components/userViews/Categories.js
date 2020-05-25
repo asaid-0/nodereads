389d4c6f3b1a326a137b2a400c26750a947644d9
Mohamed Feeshar <mohamed.feeshar@gmail.com>
2020-05-25 10:50:53 +0200

    show categorys to user view

--- client/src/components/userViews/Categories.js
+++ client/src/components/userViews/Categories.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect }from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
 import WithUserHeaders from '../../HOC/WithUserHeaders'
+import Category from './Category'
 
 function Categories() {
+
+    const [categories, setCategories] = useState([]);
+    
+    useEffect(() => {
+        axios.get('/categories')
+        .then(res => {
+            setCategories(res.data)
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, [])
+
     return (
+        
         <div>
-            <h1>Categories</h1>
+            <Container>   
+                <h1>Categories</h1>
+                <Row>
+                {
+                    categories.map((category) => 
+                        <Col key={category._id} className="col-12 col-md-4" style={{margin:'10px', textAlign: 'center'}} >
+                            <Category category={category}/>
+                        </Col>
+                    )
+                }
+                </Row>
+            </Container>
         </div>
     )
 }
