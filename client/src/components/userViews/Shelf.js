beda581907d245986aa7b1acb383fdf398f3cd32
Mohamed Feeshar <mohamed.feeshar@gmail.com>
2020-05-25 23:19:11 +0200

    fix book's shelf and show current book's shelf

--- client/src/components/userViews/Shelf.js
+++ client/src/components/userViews/Shelf.js
@@ -1,55 +1,50 @@
-import React, { useState, useEffect, useContext     }from 'react'
-import { UserContext } from '../authComponents/authContext';
+import React, { useState, useEffect, useContext}from 'react'
 import axios from '../../components/api/axios';
 
 const Shelf = ({book}) => {
-    
-    const { user } = useContext(UserContext);
-    const options = ["add to shelf", "want to read", "read", "reading", "drop"]
-    console.log(user)
-    const bookShelf = user.books ? user.books.find(exist).shelf || "add to shelf" : "add to shelf";
-    const [shelf, setShelf] = useState(bookShelf)
+    const options = ["add to shelf", "want to read", "read", "reading"]
+    const [shelf, setShelf] = useState("")
 
-    function exist(selectedBook){
-        return selectedBook.book == book;
-    }
+    useEffect(() => {
+        axios.get(`http://localhost:5000/home/books/${book}`)
+        .then(res => {
+            console.log(res.data)
+            setShelf(res.data.shelf)
+        })
+        .catch(err =>{
+            setShelf(options[0])
+            console.log("New Book to Shelf")
+            console.log(err);
+        })
+    });
 
     const handleChange = (event) => {
-        //send request here and if success change shelf else return error
         let action = event.target.value
         const payload = {
             "type": "shelf",
             "shelf": action
         }
-        console.log(action)
-        if(action == "drop"){
-            console.log("do someting")
-            axios.delete(`/books/${book}`, { data: payload })
+    
+        if(action == "add to shelf"){
+            axios.delete(`http://localhost:5000/books/${book}`, { data: payload })
             .then(res => {
                 setShelf(action)
-                console.log(res)
                 console.log("deleted from shelf", shelf)
             })
             .catch(err =>{
                 console.log(err);
             })
-            console.log("do someting2")
         } else {
-            
-            axios.post(`/books/${book}`, payload)
+            axios.post(`http://localhost:5000/books/${book}`, payload)
             .then(res => {
                 setShelf(action)
-                console.log(res)
                 console.log("added to shelf", shelf)
             })
             .catch(err =>{
                 console.log(err);
             })
         }
-        
     }
-    
-    console.log(book)
 
     return(
         <form>
