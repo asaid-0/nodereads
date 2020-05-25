3572b8561150d7f3460929cfd6843ad9673cb0db
Mohamed Feeshar <mohamed.feeshar@gmail.com>
2020-05-25 14:18:56 +0200

    remove book from shelf -not compelete-

--- client/src/components/userViews/Shelf.js
+++ client/src/components/userViews/Shelf.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect, useContext     }from 'react'
 import { UserContext } from '../authComponents/authContext';
-
-import axios from 'axios'
+import axios from '../../components/api/axios';
 
 const Shelf = ({book}) => {
     
     const { user } = useContext(UserContext);
-    const options = ["add to shelf", "want to read", "read", "reading"]
-    console.log(user.books)
+    const options = ["add to shelf", "want to read", "read", "reading", "drop"]
+    console.log(user)
     const bookShelf = user.books ? user.books.find(exist).shelf || "add to shelf" : "add to shelf";
     const [shelf, setShelf] = useState(bookShelf)
 
@@ -17,17 +16,36 @@ const Shelf = ({book}) => {
 
     const handleChange = (event) => {
         //send request here and if success change shelf else return error
-        event.preventDefault()
         let action = event.target.value
-        axios.post(`/books/${book}`, 
-        { "type": "shelf", "shelf": action })
-        .then(res => {
-            setShelf(action)
-            console.log("added to shelf", shelf)
-        })
-        .catch(err =>{
-            console.log(err);
-        })
+        const payload = {
+            "type": "shelf",
+            "shelf": action
+        }
+        console.log(action)
+        if(action == "drop"){
+            console.log("do someting")
+            axios.delete(`/books/${book}`, { data: payload })
+            .then(res => {
+                setShelf(action)
+                console.log(res)
+                console.log("deleted from shelf", shelf)
+            })
+            .catch(err =>{
+                console.log(err);
+            })
+            console.log("do someting2")
+        } else {
+            
+            axios.post(`/books/${book}`, payload)
+            .then(res => {
+                setShelf(action)
+                console.log(res)
+                console.log("added to shelf", shelf)
+            })
+            .catch(err =>{
+                console.log(err);
+            })
+        }
         
     }
     
