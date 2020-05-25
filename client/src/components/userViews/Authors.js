0f571be7856ca9ab8690bce6f5224567c8fbcf22
Mohamed Feeshar <mohamed.feeshar@gmail.com>
2020-05-25 14:17:56 +0200

    show authors

--- client/src/components/userViews/Authors.js
+++ client/src/components/userViews/Authors.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect }from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+import axios from '../../components/api/axios';
 import WithUserHeaders from '../../HOC/WithUserHeaders'
+import AuthorCard from './AuthorCard'
 
 function Authors() {
+
+    const [authors, setAuthors] = useState([])
+
+    useEffect(() => {
+        async function fetchAuthors() {
+            const result = await axios.get('/authors')
+            if(result.data){
+                setAuthors(result.data.data)
+            }else{
+                console.log("something went wrong")
+            }
+        }
+        fetchAuthors();
+      }, []);
+
     return (
         <div>
             <h1>Authors</h1>
+            <Row>
+                {
+                    authors.length ? authors.map((author) => 
+                        <Col key={author._id} className="col-12 col-md-4" style={{margin:'10px', textAlign: 'center'}} >
+                            <AuthorCard author={author}/>
+                        </Col>
+                    ) :
+                    <h2>No Authors</h2>
+                }
+            </Row>
         </div>
     )
 }
